852e8d9921fb8dedf8b7063be53f484a347a87c9
Gianfranco <cicciomesere@gmail.com>
2025-08-12 21:57:03 +0100

    Merged files and incredibly wip knockout

--- lua/autorun/mgs4_init.lua
+++ lua/autorun/mgs4_init.lua
@@ -1,3 +1,112 @@
+local ent = FindMetaTable("Entity")
+
+-- === Helpers ===
+function ent:SetSVAnimation(anim, autostop)
+    if not self then return end
+
+    self:SetNWString('SVAnim', anim)
+    self:SetNWFloat('SVAnimDelay', select(2, self:LookupSequence(anim)))
+    self:SetNWFloat('SVAnimStartTime', CurTime())
+    self:SetCycle(0)
+    if autostop then
+        local delay = select(2, self:LookupSequence(anim))
+        timer.Simple(delay, function()
+            if !IsValid(self) then return end
+
+            local anim2 = self:GetNWString('SVAnim')
+
+            if anim == anim2 then
+                self:SetSVAnimation("")
+            end
+        end)
+    end
+end
+
+function ent:SVAnimationPrep(anim, callback)
+    if not self then return end
+    
+    local current_anim = self:LookupSequence(anim)
+    local duration = self:SequenceDuration(current_anim)
+
+    local prevWeapon
+    local prevWeaponClass
+
+    self:SetNW2Bool("animation_playing", true)
+
+    if self:IsPlayer() then
+        prevWeapon = self:GetActiveWeapon()
+        prevWeaponClass = prevWeapon:GetClass()
+        self:SetActiveWeapon( NULL )
+        self:Freeze(true)
+    else
+        self:SetCondition( COND.NPC_FREEZE )
+    end
+
+    self:SetVelocity(-self:GetVelocity())
+
+    timer.Simple(duration, function()
+        
+        if self:IsPlayer() then
+            self:Freeze(false)
+
+            if ( !prevWeapon:IsValid() ) then
+                prevWeapon = self:Give( prevWeaponClass )
+            end
+
+            self:SelectWeapon( prevWeapon )
+        else
+            self:SetCondition( COND.NPC_UNFREEZE )
+        end
+
+        self:SetNW2Bool("animation_playing", false)
+
+        if callback and type(callback) == "function" then
+            callback(self)
+        end
+
+    end)
+end
+
+function ent:Knockout()
+    if not self then return end
+
+    self:SetNW2Bool("is_knocked_out", true)
+
+    local knockout_type = self:GetNW2Int("last_nonlethal_damage_type", 0)
+    local crouched
+
+    if self:IsPlayer() then
+        crouched = self:Crouching()
+    else
+        crouched = false
+    end
+
+    if knockout_type == 0 then
+        self:SetSVAnimation("mgs4_knocked_out_loop_faceup", true)
+        self:SVAnimationPrep("mgs4_knocked_out_loop_faceup")
+    elseif knockout_type == 1 then
+        if crouched then
+            self:SetSVAnimation("mgs4_sleep_crouched", true)
+            self:SVAnimationPrep("mgs4_sleep_crouched")
+        else
+            self:SetSVAnimation("mgs4_sleep", true)
+            self:SVAnimationPrep("mgs4_sleep")
+        end
+    elseif knockout_type == 2 then
+        if crouched then
+            self:SetSVAnimation("mgs4_stun_crouched", true)
+            self:SVAnimationPrep("mgs4_stun_crouched")
+        else
+            self:SetSVAnimation("mgs4_stun", true)
+            self:SVAnimationPrep("mgs4_stun")
+        end
+    end
+end
+
+function KnockoutLoop(ent)
+
+end
+
 hook.Add("OnEntityCreated", "MGS4EntitySpawn", function(ent)
     --- Ensure only valve biped models are affected
     if ent:LookupBone("ValveBiped.Bip01_Pelvis") == nil then return end
@@ -16,17 +125,18 @@ hook.Add("OnEntityCreated", "MGS4EntitySpawn", function(ent)
     --- 2 = Advanced (Adds holds and higher stun damage)
     --- 3 = Expert (Adds hold abilities such as the sop scanner and higher stun damage)
     --- 4 = Master (Adds counters anyone with a lower CQC level and maximum stun damage)
-    ent:SetNW2Int("cqc_level", 0)
+    ent:SetNW2Int("cqc_level", 4)
 
     --- Psyche
     --- If it reaches 0, the entity will be knocked out
     --- Only regenerates when knocked out or if reading a magazine
     ent:SetNW2Float("psyche", 100)
+    ent:SetNW2Bool("is_knocked_out", false)
 
     ---- Last Non-Lethal Damage Type
-    --- 0 = Generic Stun
+    --- 0 = CQC Stun
     --- 1 = Tranquilizers
-    --- 2 = CQC Stun
+    --- 2 = Generic Stun
     ent:SetNW2Int("last_nonlethal_damage_type", 0)
 end)
 
@@ -91,6 +201,44 @@ hook.Add("PlayerPostThink", "MGS4CQCCheck", function(ply)
     ply:SetNW2Entity("cqc_target", cqc_target)
 end)
 
+-- Check for entities that have reached 0 psyche
+hook.Add("Think", "MGS4PsycheCheck", function()
+    for _, ent in ipairs(ents.GetAll()) do
+        if ent:GetNW2Float("psyche", 100) <= 0 and not ent:GetNW2Bool("is_knocked_out", false) then
+            ent:Knockout() -- Knock out the player silently
+        end
+
+        if ent:GetNW2Bool("is_knocked_out", true) and ent:GetNW2Float("psyche", 100) <= 100 then
+            ent:SetNW2Bool("animation_playing", true)
+
+            local knockout_type = ent:GetNW2Int("last_nonlethal_damage_type", 0)
+
+            if knockout_type == 2 then
+                ent:SetSVAnimation("mgs4_knocked_out_loop_faceup", true)
+            else
+                ent:SetSVAnimation("mgs4_knocked_out_loop_facedown", true)
+            end
+
+            local psyche = ent:GetNW2Float("psyche", 100)
+            if psyche < 100 then
+                psyche = psyche + GetConVar("mgs4_psyche_recovery"):GetFloat() * FrameTime()
+                ent:SetNW2Float("psyche", math.min(psyche, 100)) -- Cap at 100
+            end
+
+            if ent:IsPlayer() and ent:KeyPressed(IN_USE) then
+                psyche = psyche + GetConVar("mgs4_psyche_recovery_action"):GetFloat()
+                ent:SetNW2Float("psyche", math.min(psyche, 100)) -- Cap at 100
+            end
+        
+        end
+
+        if ent:GetNW2Bool("is_knocked_out", true) and ent:GetNW2Float("psyche", 100) >= 100 then
+            ent:SetNW2Bool("is_knocked_out", false)
+            ent:SetNW2Float("psyche", 100)
+        end
+    end
+end)
+
 hook.Add("CalcMainActivity", "!MGS4Anims", function(ply, vel)
     local str = ply:GetNWString('SVAnim')
     local num = ply:GetNWFloat('SVAnimDelay')
@@ -101,3 +249,96 @@ hook.Add("CalcMainActivity", "!MGS4Anims", function(ply, vel)
         return -1, current_anim
     end
 end)
+
+
+-- === CQC ===
+
+function Cqc_check(ply)
+    if not IsValid(ply) or not ply:IsPlayer() then return end
+
+    local is_in_cqc = ply:GetNW2Bool("is_in_cqc", false)
+    local cqc_target = ply:GetNW2Entity("cqc_target", Entity(0))
+
+    if is_in_cqc then return end
+
+    if ply:IsOnGround() and !IsValid(cqc_target) then
+        Cqc_fail(ply)
+    elseif ply:IsOnGround() and IsValid(cqc_target) and cqc_target:IsOnGround() then
+        Cqc_throw(ply, cqc_target)
+    end
+end
+
+-- Fail, play anim to punish the player
+function Cqc_fail(ply)
+    local crouched = ply:Crouching()
+    if crouched then
+        ply:SetSVAnimation("mgs4_cqc_fail_crouched", true)
+    else
+        ply:SetSVAnimation("mgs4_cqc_fail", true)
+    end
+
+    ply:SetNW2Bool("is_in_cqc", true)
+
+    ply:SVAnimationPrep("mgs4_cqc_fail", function()
+        ply:SetNW2Bool("is_in_cqc", false)
+        -- Move player slightly forward
+        if !crouched then
+            local forward = ply:GetForward()
+            ply:SetPos(ply:GetPos() + forward * 20)
+        end
+    end)
+
+end
+
+-- CQC Throw mechanic
+function Cqc_throw(ply, target)
+    -- Ensure target is facing the player
+    local player_pos = ply:GetPos()
+    local player_angle = ply:GetAngles()
+    
+    target:SetPos(player_pos + (player_angle:Forward() * 30)) -- Move the target slightly forward
+
+    if target:IsPlayer() then
+        target:SetEyeAngles(player_angle + Angle(0, 180, 0)) -- Set the target's eye angles to face the player
+    else
+        -- For NPCs, we can use a different method to ensure they face the player
+        target:SetAngles(player_angle)
+    end
+
+    ply:SetNW2Bool("is_in_cqc", true)
+
+    ply:SVAnimationPrep("mgs4_cqc_throw", function()
+        ply:SetNW2Bool("is_in_cqc", false)
+        
+        -- Look to the left
+        ply:SetEyeAngles(ply:GetAngles() + Angle(0, 90, 0))
+    end)
+
+    target:SetNW2Int("last_nonlethal_damage_type", 0)
+
+    target:SVAnimationPrep("mgs4_cqc_throw_victim", function()
+        target:SetNW2Bool("is_in_cqc", false)
+
+        target:SetPos(ply:GetPos() + (ply:GetRight() * -30)) -- Move the target slightly forward
+
+        if target:IsPlayer() then
+            target:SetEyeAngles(ply:GetAngles()) -- Set the target's eye angles to face away from the player
+        else
+            -- For NPCs, we can use a different method to ensure they face away from the player
+            target:SetAngles(ply:GetAngles())
+        end
+
+        -- CQC level stun damage
+        local cqc_level = ply:GetNW2Int("cqc_level", 0)
+
+        target:SetNW2Float("psyche", 25 * cqc_level)
+
+    end)
+    
+    ply:SetSVAnimation("mgs4_cqc_throw", true)
+    target:SetSVAnimation("mgs4_cqc_throw_victim", true)
+
+end
+
+-- Custom commands
+concommand.Add("mgs4_cqc_throw", Cqc_check)
